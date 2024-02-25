3431a46935705d3ad1925c23d6030573f76a8121
Kaywuyep <wuyepkate@gmail.com>
2024-02-25 12:55:33 -0800

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Init file for views module it contains the blue print for the API"""
 from flask import Blueprint
-from api.v1.views.index import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
+from api.v1.views.index import *
 # from api.v1.views.states import *
 # from api.v1.views.cities import *
 # from api.v1.views.amenities import *
