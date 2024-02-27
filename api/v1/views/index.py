8501732988d135c85609d87c89c0bce18b2e18d8
Kaywuyep <wuyepkate@gmail.com>
2024-02-27 00:10:09 -0800

    fix task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,8 @@
 This module contains route status
 """
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -15,17 +14,18 @@ def status():
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def stats():
     """
     Counts the number of objects
     """
     objects = {
-            "amenities": storage.count('Amenity'),
-	    "cities": storage.count('City'),
-	    "places": storage.count('Place'),
-	    "reviews": storage.count('Review'),
-	    "states": storage.count('State'),
-	    "users": storage.count('User'),
-	     }
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
+    }
     return jsonify(objects)
